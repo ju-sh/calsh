fdfc1055259a53b62f3f4197d297fd14dc76fe75
Julin S <48789920+ju-sh@users.noreply.github.com>
2020-06-17 11:32:01 +0530

    Change return value of a function

--- cal_sh.sh
+++ cal_sh.sh
@@ -17,11 +17,11 @@
 
 function is_pos_integer {
     # Returns zero if argument has only digits
-    # Otherwise negative value is returned
+    # Otherwise non-zero value is returned
     if (( "$#" == 0 ))
     then
         echo "Error: is_pos_integer (function): No arguments" >2
-        exit -2
+        return 1
     fi
 
     case "$1" in
@@ -31,19 +31,17 @@ function is_pos_integer {
     return 0
 }
 
-function check_month {
+function parse_month {
     # Parse month name to month number
     # Return negative value on error
     # Returns a number
     if (( "$#" == 0 ))
     then
-        echo "Error: check_month (function): No arguments" >2
-        exit -2
+        echo "Error: parse_month (function): No arguments" >2
+        echo "-1"
     fi
 
-    is_pos_integer "$1"
-    exit_code="$?"
-    if (( "$exit_code" == 0 )) && (( "$1" > 0 )) && (( "$1" < 13 ))
+    if is_pos_integer "$1" && (( "$1" > 0 )) && (( "$1" < 13 ))
     then
         echo "Debug: month_arg is a valid month num" >2
         month_num=$1
@@ -62,19 +60,17 @@ function check_month {
     echo "$month_num"   # rv
 }
 
-function check_year {
+function parse_year {
     # Check if argument is a natural number less than 9999 
     # Return 0 on error
     # Returns zero number
     if (( "$#" == 0 ))
     then
-        echo "Error: check_year (function): No arguments" >2
+        echo "Error: parse_year (function): No arguments" >2
         echo "-1"   # rv
     fi
     
-    is_pos_integer "$1"
-    exit_code="$?"
-    if (( "$exit_code" == 0 )) && (( "$1" > 0 )) && (( "$1" < 10000 ))
+    if is_pos_integer "$1" && (( "$1" > 0 )) && (( "$1" < 10000 ))
     then
         echo "Debug: year_arg is a valid year" >2
         echo "check: year $1" >2
@@ -88,47 +84,41 @@ function check_year {
 # Obtain required arguments
 if (( "$#" > 2 ))
 then
-    echo "Error: Too many arguments" >2
+    echo "Error: Too many arguments"
     exit 1
 elif (( "$#" == 2 ))
 then
     month_arg=$1
     year_arg=$2
-    #echo "two pos arg"
 elif (( "$#" == 1 ))
 then
     month_arg=$(date +%m)
     year_arg=$1
-    #echo "one pos arg"
 elif (( "$#" == 0 ))
 then
     month_arg=$(date +%m)
     year_arg=$(date +%Y)
-    #echo "No pos args" >2
 fi
 
 echo "$month_arg, $year_arg" >2
 
 # Parse month
-month=$(check_month "$month_arg")
+month=$(parse_month "$month_arg")
 if (( month == -1 ))
 then
-    echo "Error: $month_arg is neither a month number (1..12) nor a name" >2
+    echo "Error: $month_arg is neither a month number (1..12) nor a name"
     exit 1
 fi
 
 # Parse year
-year=$(check_year "$year_arg")
-echo "check_year_rv: $year" >2
+year=$(parse_year "$year_arg")
 if (( year == 0 ))
 then
-    echo "Error: year $year not in range 1..9999" >2
+    echo "Error: year $year not in range 1..9999"
     exit 1
 fi
 
-
-echo "$month, $year" 
-read -rn1
+echo "$month, $year" >2
 
 # The event loop
 response='a'
@@ -218,58 +208,3 @@ do
         echo "k!" >2
     fi
 done
-#echo $response
-
-#jk=>month
-#hl=>year
-#HL=>Decade
-##C-f,C-b=>century
-
-
-##################
-
-
-## Obtain required arguments
-#if (( "$#" > 2 ))
-#then
-#    echo "Error: Too many arguments"
-#    exit 1
-#elif (( "$#" == 2 ))
-#then
-#    month=$1
-#    year=$2
-#    echo "two pos arg"
-#elif (( "$#" == 1 ))
-#then
-#    year=$1
-#    echo "one pos arg"
-#elif (( "$#" == 0 ))
-#then
-#    year=$(date +%Y)
-#    month=$(date +%m)
-#    echo "No pos args"
-#fi
-#
-#
-#echo $year
-#echo $month
-
-
-##################
-
-
-# for arg in "$@"
-# do
-#     # if the quotes are dropped $arg would be
-#     # executed as a command
-#     echo "$arg"  
-# done
-
-
-##################
-
-
-# one arg => year (num)
-# two arg => 
-#  - arg1: month (num)
-#  - arg2: year (num)
